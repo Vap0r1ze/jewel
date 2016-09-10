645bfa881bbf3b1da6009fa9a1c89e31049b0f41
Vap0r1ze <superdash993@gmail.com>
2016-09-10 00:10:18 -0400

    Update source.js

--- commands/source.js
+++ commands/source.js
@@ -1,6 +1,17 @@
+const fs = require("fs");
+
 module.exports = {
 	main: function(Bot, m, args) {
-		Bot.createMessage(m.channel.id, "Here is my source code: https://github.com/Vap0r1ze/Jewel")
+		if (args != "") {
+			var commands = fs.readdirSync("./commands/");
+			if (commands.indexOf(args+".js") > -1) {
+				Bot.createMessage(m.channel.id, "Here is the source for the command **"+args+"** https://github.com/Vap0r1ze/Jewel/tree/master/commands/"+args+".js");
+			} else {
+				Bot.createMessage(m.channel.id, "That command doesn't exist.");
+			}
+		} else {
+			Bot.createMessage(m.channel.id, "Here is my source code: https://github.com/Vap0r1ze/Jewel")
+		}
 	},
 	help: "Shows a link to my public GitHub page"
 }
