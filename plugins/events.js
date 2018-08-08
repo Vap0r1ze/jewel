4c693389c60acfa8433dd0752f6c8ebc5d3e2401
Vap0r1ze <superdash993@gmailc.om>
2018-08-08 10:08:03 -0400

    add events

--- plugins/events.js
+++ plugins/events.js
@@ -1,33 +1,25 @@
 let e = module.exports = {}
 
 const fs = require('fs')
+const chalk = require('chalk')
 
 e.dependencies = ['discordjs']
 
-// e.init = function (Bot) {
-//     Bot.client.on('message', msg => {
-//     if (msg.content.startsWith(Bot.config.main.prefix)) {
-//       if (!msg.channel.guild) return
-//       if (msg.author.bot) return
-//       let args = msg.content
-//         .replace(Bot.config.main.prefix, '')
-//         .split(/\s+/)
-//         .filter(Boolean)
-//       let command = args.shift().toLowerCase()
-//       if (!/[\w\-]/.test(command)) return
-//       if (fs.existsSync(`commands/${command}.js`)) {
-//         let cmd = require(`../commands/${command}.js`)
-//         Bot.util.logger.cmd(command, msg.author)
-//         try {
-//           if (e.checkperms(cmd.perms, msg.author)) {
-//             cmd.run.bind(Bot)(msg, args)
-//           } else {
-//             msg.channel.send(`You must be a **${cmd.perms}** to do this!`)
-//           }
-//         } catch (err) {
-//           Bot.util.logger.err('CMD', err)
-//         }
-//       }
-//     }
-//   })
-// }
+e.init = function (Bot) {
+  if (!fs.existsSync('events'))
+    fs.mkdirSync('events')
+  let events = fs.readdirSync('events')
+  let h = 0
+  for (let event of events) {
+    let handlers = Bot.util.getFiles(`events/${event}`)
+    h += handlers.length
+    Bot.on(event, function (...args) {
+      handlers.forEach(h => {
+        h(...args)
+      })
+    })
+  }
+  let e = events.length
+  Bot.util.logger.log('EVT', chalk`Listening to {green.bold ${e}} event${e===1?'':'s'}`
+    + chalk` with {green.bold ${h}} handler${h===1?'':'s'}`)
+}
