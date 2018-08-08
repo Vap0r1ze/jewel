cc9c94878b04f89ab26c7037fcbc53f2c75ea043
Vap0r1ze <superdash993@gmailc.om>
2018-08-08 13:30:39 -0400

    less verbose redis errors

--- plugins/redis.js
+++ plugins/redis.js
@@ -6,7 +6,7 @@ Promise.promisifyAll(redis)
 e.init = function (Bot) {
   let r = redis.createClient(process.env.REDIS_PORT)
   r.on('error', err => {
-    return Bot.util.logger.error('REDIS', err)
+    return Bot.util.logger.error('REDIS', err.message)
   })
   r.on('connect', () => {
     Bot.util.logger.log('REDIS', 'Connected to server')
