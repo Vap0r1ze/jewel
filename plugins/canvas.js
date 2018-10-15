7849176fc26ea5695e3ecc304c778b6ed2017fdc
Vap0r1ze <superdash993@gmail.com>
2018-10-15 19:04:33 -0400

    remove profile

--- plugins/canvas.js
+++ plugins/canvas.js
@@ -41,12 +41,6 @@ CanvasRenderingContext2D.prototype.wrapText = function (text, x, y, width, lineH
   return this
 }
 
-const bg = '#1e293a'
-const textMuted = '#487098'
-
-const accent1 = '#fa489a'
-const accent2 = '#00eec5'
-
 e.init = function (Bot) {
   registerFont(path.join(__dirname, '../fonts/Lato-Regular.ttf'), {
     family: 'Lato',
@@ -71,78 +65,6 @@ e.init = function (Bot) {
         }
         img.src = url
       })
-    },
-    async profileCard (user) {
-      let canvas = createCanvas(550, 400)
-      let ctx = canvas.getContext('2d')
-
-      // Global Clip
-      ctx.roundRect(0, 0, 550, 400, 10)
-      ctx.clip()
-
-      // Background
-      ctx.fillStyle = bg
-      ctx.fillRect(0, 0, 550, 400)
-
-      // Avatar
-      let avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`
-      ctx.beginPath()
-      ctx.moveTo(0, 0)
-      ctx.lineTo(250, 0)
-      ctx.lineTo(250, 400)
-      ctx.lineTo(0, 400)
-      ctx.closePath()
-      ctx.save()
-      ctx.clip()
-      ctx.drawImage(await this.getImage(avatar), -47.5, 0, 345, 345)
-      ctx.restore()
-
-      // Status
-      ctx.fillStyle = accent2
-      ctx.shadowColor = textMuted
-      ctx.shadowBlur = 4
-      ctx.beginPath()
-      ctx.arc(525, 22, 3, 0, Math.PI*2)
-      ctx.fill()
-      ctx.shadowBlur = 0
-      ctx.fillStyle = textMuted
-      ctx.textBaseline = 'middle'
-      ctx.textAlign = 'right'
-      ctx.fillText('online', 515, 21)
-      ctx.textAlign = 'left'
-
-      // Username
-      ctx.fillStyle = accent1
-      ctx.font = '800 30px Lato'
-      ctx.textBaseline = 'hanging'
-      ctx.fillText(user.username, 270, 30)
-
-      // Role
-      ctx.fillStyle = textMuted
-      ctx.font = '500 15px Lato'
-      ctx.textBaseline = 'hanging'
-      ctx.fillText(user.role, 270, 65)
-
-      // Bio
-      ctx.fillStyle = accent2
-      ctx.font = '500 25px Lato'
-      ctx.textBaseline = 'hanging'
-      ctx.fillText('Bio', 270, 90)
-      ctx.fillStyle = textMuted
-      ctx.font = '500 15px Lato'
-      ctx.textBaseline = 'hanging'
-      ctx.wrapText(user.bio, 270, 120, 260, null, 5)
-
-      // Footer
-      ctx.fillStyle = accent1
-      ctx.beginPath()
-      ctx.moveTo(0, 290)
-      ctx.lineTo(0, 400)
-      ctx.lineTo(545, 400)
-      ctx.closePath()
-      ctx.fill()
-
-      return canvas.toBuffer()
     }
   }
 }
