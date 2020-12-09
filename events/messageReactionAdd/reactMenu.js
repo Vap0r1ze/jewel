28e821954cee56366423b898516b4c6ff809f8fa
Vap0r1ze <superdash993@gmail.com>
2020-12-09 12:00:22 -0500

    reactMenu: improvements + bug fix

--- events/messageReactionAdd/reactMenu.js
+++ events/messageReactionAdd/reactMenu.js
@@ -1,12 +1,12 @@
-module.exports = function (message, { name: emojiName, id: emojiId }, member) {
-  if (member.id === this.client.user.id || member.bot) return
+module.exports = function (message, { name: emojiName, id: emojiId }, user) {
+  if (user.id === this.client.user.id) return
   const menuInfo = this.menus[message.id]
   if (menuInfo) {
     for (const [ choiceId, emoji ] of Object.entries(menuInfo.emojis)) {
       if (emoji === (emojiId ? `${emojiName}:${emojiId}` : emojiName)) {
         const handler = this.util.accessObjPath(this, menuInfo.handlerPath, true)
         try {
-          handler(choiceId, member.id, false)
+          handler(choiceId, user.id, false)
         } catch (error) {
           this.util.logger.error(`MENU:${menuInfo.handlerPath}:${choiceId}`, error)
         }
--- events/messageReactionRemove/reactMenu.js
+++ events/messageReactionRemove/reactMenu.js
@@ -1,12 +1,12 @@
-module.exports = function (message, { name: emojiName, id: emojiId }, member) {
-  if (member.id === this.client.user.id || member.bot) return
+module.exports = function (message, { name: emojiName, id: emojiId }, userId) {
+  if (userId === this.client.user.id) return
   const menuInfo = this.menus[message.id]
   if (menuInfo) {
     for (const [ choiceId, emoji ] of Object.entries(menuInfo.emojis)) {
       if (emoji === (emojiId ? `${emojiName}:${emojiId}` : emojiName)) {
         const handler = this.util.accessObjPath(this, menuInfo.handlerPath, true)
         try {
-          handler(choiceId, member.id, true)
+          handler(choiceId, userId, true)
         } catch (error) {
           this.util.logger.error(`MENU:${menuInfo.handlerPath}:${choiceId}`, error)
         }
