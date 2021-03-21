e1189dd04425dbc79e8ed8d70dc2ce1b8c475393
Justice Almanzar <superdash993@gmail.com>
2021-03-21 18:51:30 -0400

    scrobbles: expire data

--- src/plugins/scrobbles.ts
+++ src/plugins/scrobbles.ts
@@ -2,6 +2,7 @@ import superagent from 'superagent'
 import btoa from 'btoa'
 import Bot from '@/services/Bot'
 import chalk from 'chalk'
+import moment from 'moment'
 
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env
 
@@ -66,6 +67,7 @@ export default function initScrobbles(this: Bot) {
   const maxHistory = 100
 
   this.api.get('/scrobbles', async (request, reply) => {
+    const now = Date.now()
     reply.type('application/json').code(200)
     const responses: ScrobbleData[] = []
     if (typeof request.query !== 'object' || !request.query) return []
@@ -86,13 +88,20 @@ export default function initScrobbles(this: Bot) {
         }
       })
     }
-    return responses
+
+    return responses.filter(data => {
+      if (data.scrobbles[0].endTime) {
+        return moment(now).diff(data.scrobbles[0].endTime, 'days') <= 7
+      }
+      return moment(now).diff(data.scrobbles[0].startTime, 'hours') <= 24
+    })
   })
 
   this.api.get('/scrobbles/spotify-token', async (request, reply) => {
+    const now = Date.now()
     reply.type('application/json').code(200)
     if (cache.spotifyToken) {
-      if (cache.spotifyToken.expiresAt <= Date.now()) cache.spotifyToken = null
+      if (cache.spotifyToken.expiresAt <= now) cache.spotifyToken = null
     }
     if (!cache.spotifyToken) {
       try {
@@ -102,7 +111,7 @@ export default function initScrobbles(this: Bot) {
           // eslint-disable-next-line @typescript-eslint/camelcase
           grant_type: 'client_credentials',
         }, 'form')
-        const expiresAt = Date.now() + (data.expires_in - 5) * 1000
+        const expiresAt = now + (data.expires_in - 5) * 1000
         cache.spotifyToken = {
           token: data.access_token,
           expiresAt,
