702e66f1db5d609c7864e5a42335542922157004
Vap0r1ze <superdash993@gmail.com>
2023-08-03 14:32:15 -0400

    debug

--- src/events/channelPinUpdate/topPin.ts
+++ src/events/channelPinUpdate/topPin.ts
@@ -3,5 +3,6 @@ import Bot from '@/services/Bot'
 import { TextableChannel } from 'eris'
 
 export default function topPin(this: Bot, channel: TextableChannel) {
+    console.log('channelPinUpdate')
     if (channel.type === 0) doTopPinCheck(this, channel)
 }
